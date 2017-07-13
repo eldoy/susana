9ebe81c81b7747c990d46c6b1a68ec2f7ea23911
Vidar <vidar@fugroup.net>
2017-07-13 11:58:57 +0200

    Fixed install script

--- gem/lib/susana.rb
+++ gem/lib/susana.rb
@@ -6,6 +6,7 @@
 # This script downloads and installs your new application
 
 require 'securerandom'
+require 'fileutils'
 
 puts "\n * S U S A N A *\n\n"
 
@@ -18,25 +19,22 @@ if !name or name == ''
   exit
 end
 
-# Get working dir
-dir = Dir.pwd
-
 # Install new susana app
 `git clone https://github.com/fugroup/susana #{name}`
 
-# Change working dir
-Dir.chdir(name)
+FileUtils.cd(name)
 
 # Cleanup
-`rm -rf .git`
-`rm -rf gem`
+FileUtils.remove_dir(".git")
+FileUtils.remove_dir("gem")
 
 # Create cookie secret
 secret = SecureRandom::hex(26)
 
 # Install secret
 key = 'a8976c0df4fb8a28f8fe979270443d37f239cf8da8f1bf9734ba'
-File.open('config.ru', 'w'){|f| f.write(File.read('config.ru').gsub(key, secret))}
+file = File.read("config.ru").gsub(key, secret)
+File.open("config.ru", 'w'){|f| f.write(file)}
 
 puts "\nApp #{name} created, please review your settings in the config directory\n\n"
 puts "To start the application, go to the #{name} directory and run 'puma'\n"
